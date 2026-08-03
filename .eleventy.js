30b7ad34c9b45dff3325c64d5ea29fa94f7e9505
LazloFuches <robert.egert@gmail.com>
2026-08-02 20:00:14 -0400

    image detail pages: clean rebuild, working baseline

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,27 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
-  
+
+  eleventyConfig.addCollection("artworks", function(collectionApi) {
+    const artworks = [];
+    const projectPages = collectionApi.getFilteredByGlob("src/projects/*.njk")
+      .filter(p => p.data.images && p.data.images.length > 0);
+
+    for (const project of projectPages) {
+      project.data.images.forEach((img, i) => {
+        artworks.push({
+          ...img,
+          projectTitle: project.data.title,
+          projectYear: project.data.year,
+          projectSlug: project.fileSlug,
+          index: i,
+          total: project.data.images.length,
+        });
+      });
+    }
+    return artworks;
+  });
+
   return {
     dir: {
       input: "src",
